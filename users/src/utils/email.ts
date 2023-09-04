4bdebcd9f016557eb4dfd57e0951a64380562480
Bhargav-Synfosys <110816674+Bhargav-Synfosys@users.noreply.github.com>
2023-09-04 12:25:05 +0530

    sending signup email successfully without any errors

--- users/src/utils/email.ts
+++ users/src/utils/email.ts
@@ -6,30 +6,33 @@ class emailService {
     to: (string | Address)[],
     html: string,
     subject: string
-  ): Promise<any> => {
-    const transporter = nodemailer.createTransport({
-      host: process.env.emailhost,
-      port: 465,
-      auth: {
-        user: process.env.fromemail,
-        pass: process.env.password,
-      },
-      secure: true,
-    });
-    const details = {
-      html: html,
-      from: process.env.fromemail!,
-      to: to,
-      subject: subject,
-    };
-    await transporter.sendMail(details, (error, info) => {
-      if (error) {
-        console.error("Error sending an email");
-        return null;
-      } else {
-        console.log("Sent email successfully", info.messageId);
-        return info.messageId;
-      }
+  ): Promise<string | null> => {
+    // Create a promise to wrap the sendMail callback
+    return new Promise((resolve, reject) => {
+      const transporter = nodemailer.createTransport({
+        host: process.env.emailhost,
+        port: 465,
+        auth: {
+          user: process.env.fromemail,
+          pass: process.env.password,
+        },
+        secure: true,
+      });
+      const details = {
+        html: html,
+        from: process.env.fromemail!,
+        to: to,
+        subject: subject,
+      };
+      transporter.sendMail(details, (error, info) => {
+        if (error) {
+          console.error("Error sending an email");
+          reject(error); // Reject the promise with the error
+        } else {
+          console.log("Sent email successfully", info.messageId);
+          resolve(info.messageId); // Resolve the promise with the messageId
+        }
+      });
     });
   };
 }
