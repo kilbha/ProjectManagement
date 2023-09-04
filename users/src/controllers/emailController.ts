5b3627907b076fb79f01745db6f57250b2bee14d
Bhargav-Synfosys <110816674+Bhargav-Synfosys@users.noreply.github.com>
2023-09-04 12:46:17 +0530

    Error validation is completed while sending signup emial

--- users/src/controllers/emailController.ts
+++ users/src/controllers/emailController.ts
@@ -1,23 +1,29 @@
 import { Request, Response } from "express";
 import emailService from "../utils/email";
+import { validationResult } from "express-validator";
 
 class emailController {
   emailservice: emailService = new emailService();
 
   send_signup_email = async (req: Request, res: Response) => {
     try {
-      const { toEmail, html, subject } = req.body;
-      let messageId = await this.emailservice.send_email(
-        toEmail,
-        html,
-        subject
-      );
-      if (messageId) {
-        res
-          .status(200)
-          .json({ message: "sent email successfully", messageId: messageId });
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        res.status(400).json({ errors: errors.array() });
       } else {
-        res.status(500).json({ message: "server error" });
+        const { toEmail, html, subject } = req.body;
+        let messageId = await this.emailservice.send_email(
+          toEmail,
+          html,
+          subject
+        );
+        if (messageId) {
+          res
+            .status(200)
+            .json({ message: "sent email successfully", messageId: messageId });
+        } else {
+          res.status(500).json({ message: "server error" });
+        }
       }
     } catch (error) {
       res.status(500).json({ message: "server error" });
--- users/src/routes/emailRoute.ts
+++ users/src/routes/emailRoute.ts
@@ -1,8 +1,17 @@
 import { Router, Request, Response } from "express";
 import emailController from "../controllers/emailController";
-
+import { body, validationResult } from "express-validator";
 const router = Router();
 const userController = new emailController();
-router.post("/signup", userController.send_signup_email);
+router.post(
+  "/signup",
+  [
+    body("toEmail").isEmail().withMessage("Invalid Email Address"),
+    body("toEmail").notEmpty().withMessage("Please provide EmailId"),
+    body("subject").notEmpty().withMessage("Please provide subject of email"),
+    body("html").notEmpty().withMessage("Please provide body of email"),
+  ],
+  userController.send_signup_email
+);
 
 export default router;
